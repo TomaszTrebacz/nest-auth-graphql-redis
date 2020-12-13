c3479e98b80305f53777ffe47c6294daffda9d5d
Tomasz Trębacz <trebacztomasz@outlook.com>
2020-12-13 21:17:01 +0100

    perf<redis-handler>: rewrite service using ES

--- index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./lib/auth-gql-redis.module";
-
-export * from "./lib/redis-handler/redis-handler.module";
-export * from "./lib/redis-handler/redis-handler.service";
--- src/redis-handler/redis-handler.service.ts
+++ src/redis-handler/redis-handler.service.ts
@@ -20,62 +20,35 @@ export class RedisHandlerService {
     return this.redisService.getClient();
   }
 
-  async saveUser({
-    id,
-    role,
-    confirmed,
-    confirmToken,
-    count,
-  }: userInfoInterface) {
-    await this.client.hmset(
-      id,
-      new Map([
-        ["role", role],
-        ["count", count],
-        ["confirmed", confirmed],
-        ["confirmtoken", confirmToken],
-      ])
-    );
+  /*
+    comprehensive function for saving new user in database
+    and create/update fields for purpopses
+    like reset password, refresh token etc.
+  */
+  async setUser(id: string, properties: Map<string, string>): Promise<boolean> {
+    try {
+      await this.client.hmset(id, properties);
+
+      return true;
+    } catch (err) {
+      throw new UnprocessableEntityException(err.message);
+    }
   }
 
-  async isUserExists(id: string): Promise<boolean> {
+  async getFields(id: string, keys: string[]) {
+    const values = await this.client.hmget(id, keys);
+
+    const fields = Object.fromEntries(keys.map((_, i) => [keys[i], values[i]]));
+    return fields;
+  }
+
+  async userExists(id: string): Promise<boolean> {
     const res = await this.client.exists(id);
 
     return res === 1 ? true : false;
   }
 
-  async saveToken(id: string, token: string) {
-    await this.client.hmset(id, { refreshtoken: token });
-  }
-
-  // function for reset password by phone number
-  async saveCodeToken(id: string, token: string) {
-    await this.client.hmset(id, { codetoken: token });
-  }
-
-  // function for change password by email
-  async saveEmailToken(id: string, token: string) {
-    await this.client.hmset(id, { changepasstoken: token });
-  }
-
-  async getUser(id: string) {
-    let user = await this.client.hgetall(id);
-
-    if (user === null) {
-      throw new UnprocessableEntityException("There is no user with given Id!");
-    }
-
-    const data = {
-      role: user.role,
-      count: parseInt(user.count),
-      refreshToken: user.refreshtoken,
-      info: user,
-    };
-
-    return data;
-  }
-
-  async getValue(id: string, key: string) {
+  async getValue(id: string, key: string): Promise<string> {
     const value = await this.client.hget(id, key);
 
     if (!value) {
@@ -85,33 +58,23 @@ export class RedisHandlerService {
     return value;
   }
 
-  async changeRole({ id, role }: userInfoInterface) {
-    await this.client.hmset(id, { role: role });
-  }
-
-  async changeCount(id: string, count: string) {
-    await this.client.hmset(id, { count: count });
-  }
-
-  async confirmUser(id: string) {
-    await this.client.hmset(id, { confirmed: "true" });
-  }
-
-  async deleteKeyField(id: string, key: string): Promise<Boolean> {
+  async deleteField(id: string, key: string): Promise<boolean> {
     const isRemoved = await this.client.hdel(id, key);
 
     if (isRemoved !== 1) {
       throw new UnprocessableEntityException("There is no user with given Id!");
     }
 
-    return new Boolean(true);
+    return true;
   }
 
-  async delete(key: string) {
+  async deleteUser(key: string): Promise<boolean> {
     if (!key) {
-      throw new UnprocessableEntityException("Null key.");
+      throw new UnprocessableEntityException("Null id.");
     }
 
     await this.client.del(key);
+
+    return true;
   }
 }
