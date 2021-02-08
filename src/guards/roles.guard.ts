72211d2c985b227aa2f0faea0fc3c3e1ed0aae9c
Tomasz Trębacz <trebacztomasz@outlook.com>
2021-02-08 18:02:27 +0100

    test: add tests for roles.guard.ts

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -1,12 +1,12 @@
-import { Injectable, ExecutionContext } from "@nestjs/common";
-import { GqlAuthGuard } from "./gql-auth.guard";
-import { GqlExecutionContext } from "@nestjs/graphql";
-import { userRole } from "../enums";
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { GqlAuthGuard } from './gql-auth.guard';
+import { userRole } from '../enums';
 
 @Injectable()
 export class RolesGuard extends GqlAuthGuard {
   async canActivate(context: ExecutionContext) {
-    const roles = this.reflector.get<userRole[]>("roles", context.getHandler());
+    const roles = this.reflector.get<userRole[]>('roles', context.getHandler());
 
     // if roles array is empty, we assume that only default user has access
     if (roles.length > 0) {
@@ -15,11 +15,11 @@ export class RolesGuard extends GqlAuthGuard {
 
       const actualRole = await this.RedisHandlerService.getValue(
         request.user.id,
-        "role"
+        'role',
       );
 
       if (!roles.includes(actualRole)) {
-        throw new Error("Wrong role!");
+        throw new Error('Wrong role!');
       }
     }
 
